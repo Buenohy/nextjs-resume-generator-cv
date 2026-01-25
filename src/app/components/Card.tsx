55080bf8f516b8108ab3e7f224ca26522facd8c5
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-01-24 21:47:53 -0300

    feat: style card

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,9 +1,14 @@
 import { ReactNode } from 'react';
-
 interface CardProps {
+  title: string;
   children: ReactNode;
 }
 
-export default function Card({ children }: CardProps) {
-  return <div className="rounded-2xl bg-white p-8">{children}</div>;
+export default function Card({ title, children }: CardProps) {
+  return (
+    <div className="rounded-2xl bg-white p-8 flex flex-col gap-4 min-w-190">
+      <h1 className="text-black text-3xl">{title}</h1>
+      {children}
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,16 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div>
-          <Card>
+        <div className="flex flex-col gap-8">
+          <Card title="Personal Details">
             <Input title="Full name" placeholder="Enter your full name" />
+          </Card>
+          <Card title="Links">
+            <Input title="Linkedin" placeholder="Enter your linkedin" />
+            <Input title="Phone" placeholder="Enter your phone" />
+            <Input title="Site" placeholder="Enter your site" />
+            <Input title="Email" placeholder="Enter your email" />
+            <Input title="Github" placeholder="Enter your github" />
           </Card>
         </div>
       </main>
