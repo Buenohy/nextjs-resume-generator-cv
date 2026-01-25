61addc6df0bbaab4989a195fefa071b90726433d
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-01-24 22:52:15 -0300

    feat: ui text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Card from './components/Card';
-import Input from './components/Input';
-import Skills from './components/Skills';
+import Card from "./components/Card";
+import Input from "./components/Input";
+import Skills from "./components/Skills";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
         <div className="flex flex-col gap-8">
           <Card title="Personal Details">
             <Input title="Full name" placeholder="Enter your full name" />
@@ -18,10 +18,10 @@ export default function Home() {
             <Input title="Github" placeholder="Enter github" />
           </Card>
           <Card title="Professional Summary">
-            <Input title="Summary" placeholder="Enter your full summary" />
+            <Input title="" placeholder="Enter your full summary" />
           </Card>
           <Card title="Skills">
-            <Skills title="Skills" placeholder="Enter skill" />
+            <Skills title="" placeholder="Enter skill" />
           </Card>
           <Card title="Experience">
             <div className="grid grid-cols-3 gap-4">
@@ -33,18 +33,18 @@ export default function Home() {
               title="Summary"
               placeholder="Enter your experience summary"
             />
-            <Input
-              title="Summary"
-              placeholder="Enter your experience summary"
-            />
-            <Input
-              title="Summary"
-              placeholder="Enter your experience summary"
-            />
-            <Input
-              title="Summary"
-              placeholder="Enter your experience summary"
-            />
+            <Input title="" placeholder="Enter your experience summary" />
+            <Input title="" placeholder="Enter your experience summary" />
+            <Input title="" placeholder="Enter your experience summary" />
+          </Card>
+          <Card title="Education">
+            <Input title="" placeholder="Enter your education" />
+            <Input title="" placeholder="Enter your education" />
+          </Card>
+          <Card title="Languages">
+            <Input title="" placeholder="Enter your languages" />
+            <Input title="" placeholder="Enter your languages" />
+            <Input title="" placeholder="Enter your languages" />
           </Card>
         </div>
       </main>
