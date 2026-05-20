48140e2ec0d5bc9de5129663ec3db5bcf3b8ab31
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-05-20 14:55:27 -0300

    feat: Remove old components

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -1,29 +1,9 @@
-import Text from "@/components/Text";
-import Button from "@/components/Button";
-import Card from "@/components/Card";
-import InputText from "@/components/InputText";
+
 
 export default function page() {
   return (
     <div className="flex flex-col gap-10">
       <div>Components Page</div>
-      <div className="flex flex-col">
-        <Text as="h1" size="lg" color="primary">Text H1 lg</Text>
-        <Text as="h1" size="md" color="primary">Text H1 md</Text>
-        <Text as="h1" size="sm" color="primary">Text H1 sm</Text>
-      </div>
-      <div className="flex flex-col gap-5">
-        <Button variant="light" size="sm">Button</Button>
-        <Button variant="light" size="sm" disabled>Button Disabled</Button>
-      </div>
-      <div>
-        <Card size="sm">
-          <Text>Card Text</Text>
-        </Card>
-      </div>
-      <div>
-        <InputText placeholder="Placeholder" />
-      </div>
     </div>
   )
 }
--- src/app/job-description/page.tsx
+++ src/app/job-description/page.tsx
@@ -1,19 +1,8 @@
-import Button from "@/components/Button";
-import Card from "@/components/Card";
-import InputText from "@/components/InputText";
-import Text from "@/components/Text";
 
 export default function page() {
   return (
     <div>
-      <Text>Job Description Page</Text>
-      <Card>
-        <InputText />
-      </Card>
-      <div className="flex gap-10 items-center justify-center">
-      <Button color="primary">Enviar 1</Button>
-      <Button color="primary">Enviar 2</Button>
-      </div>
+      <div>Job Description Page</div>
     </div>
   )
 }
