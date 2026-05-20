f72cbc762b6fd1c87caca2e0130300d831aa6cae
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-05-20 00:07:26 -0300

    feat: Card component

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -1,9 +1,10 @@
 import Text from "@/components/Text";
 import Button from "@/components/Button";
+import Card from "@/components/Card";
 
 export default function page() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-10">
       <div>Components Page</div>
       <div className="flex flex-col">
         <Text as="h1" size="lg" color="primary">Text H1 lg</Text>
@@ -14,6 +15,11 @@ export default function page() {
         <Button color="secondary" size="lg">Button Primary md</Button>
         <Button color="primary" size="md">Button Primary md</Button>
         <Button color="primary" size="sm">Button Primary sm</Button>
+      </div>
+      <div>
+        <Card>
+          <Text>Card Text</Text>
+        </Card>
       </div>
     </div>
   )
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,55 @@
-import { ReactNode } from "react";
-interface CardProps {
-  title: string;
-  children: ReactNode;
+import React from 'react';
+import { tv, type VariantProps } from 'tailwind-variants';
+import { twMerge } from 'tailwind-merge';
+
+const cardVariants = tv({
+  base: 'text-white dark:text-black border bg-transparent',
+  variants: {
+    color: {
+      primary: 'border-green-primary text-black dark:text-white hover:bg-green-primary hover:text-black',
+      secondary: 'border-green-secondary text-black dark:text-white hover:bg-green-secondary hover:text-black',
+    },
+    size: {
+      sm: 'px-5 py-2.5',
+      md: 'px-7.5 py-5',
+      lg: 'px-10 py-7.5',
+    },
+  },
+  compoundVariants: [
+    {
+      size: ['sm', 'md'],
+      class: '',
+    },
+  ],
+  defaultVariants: {
+    size: 'md',
+    color: 'primary',
+  },
+});
+
+interface CardProps extends VariantProps<typeof cardVariants> {
+  as?: React.ElementType;
+  className?: string;
+  children?: React.ReactNode;
 }
 
-export default function Card({ title, children }: CardProps) {
+export default function Card({
+  as: Tag = 'div',
+  color,
+  size,
+  className,
+  children,
+  ...props
+}: CardProps) {
   return (
-    <div className="flex h-fit min-w-72.5 flex-col gap-4 rounded-2xl bg-white p-4 sm:max-w-190">
-      <h1 className="text-xl text-black">{title}</h1>
+    <Tag
+      className={twMerge(
+        cardVariants({ color, size }),
+        className
+      )}
+      {...props}
+    >
       {children}
-    </div>
+    </Tag>
   );
 }
