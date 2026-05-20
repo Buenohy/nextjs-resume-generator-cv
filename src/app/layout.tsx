9e4ab2ba18d02d1951173d86b1d7581102ce325e
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-05-20 14:50:25 -0300

    feat: Install Shadcn Ui with preset

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
-import { IBM_Plex_Mono, IBM_Plex_Sans } from "next/font/google";
+import { IBM_Plex_Mono, IBM_Plex_Sans, Oxanium, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const jetbrainsMonoHeading = JetBrains_Mono({subsets:['latin'],variable:'--font-heading'});
+
+const oxanium = Oxanium({subsets:['latin'],variable:'--font-sans'});
 
 const IbmPlexSans = IBM_Plex_Sans({
   variable: "--font-ibm-plex-sans",
@@ -24,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", oxanium.variable, jetbrainsMonoHeading.variable)}>
       <body
         className={`${IbmPlexSans.variable} ${IbmPlexMono} antialiased`}
       >
