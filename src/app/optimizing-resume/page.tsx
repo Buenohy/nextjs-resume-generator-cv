074a7f5c9805c39d6c7c1aa12dbc6e50543b4b68
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-05-20 18:24:46 -0300

    feat: Optimizing resume page

---
+++ src/app/optimizing-resume/page.tsx
@@ -0,0 +1,37 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+export default function OptimizingResumePage() {
+  return (
+    <div>
+      <div>Optimizing Resume Page</div>
+      <Card className="shadow-primary/50 shadow-lg">
+        <CardHeader>
+          <CardTitle>Optimizing Resume</CardTitle>
+          <CardDescription>Otimizando verbos e linguagem do currículo...</CardDescription>
+          <CardAction>Card Action</CardAction>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-5">
+          <h2>⚠️ [VERBO FRACO] em Resumo (Summary)</h2>
+          <h3>Original:</h3>
+          <h4>'...using...'</h4>
+          <h2>Contexto:</h2>
+          <h3>"Proactive and growth-focused Junior Front-End Developer with solid hands-on experience developing web applications using JavaScript, CSS, HTML, and the React ecosystem."</h3>
+          <h2>🚀 Sugestão ATS:</h2>
+          <h3>['OPTIMIZED', 'IMPLEMENTED', 'DEVELOPED']</h3>
+          <h2>Palavras Suspeitas:</h2>
+          <h2 className="text-destructive">todo, url </h2>
+        </CardContent>
+        <CardFooter>
+        </CardFooter>
+      </Card>
+    </div>
+  )
+}
