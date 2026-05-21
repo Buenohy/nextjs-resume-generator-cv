139773cac4f3648dcb7190f5e5707658b8087df0
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-05-21 14:58:56 -0300

    feat: Setup next-intl locale and dashboard folders and structures

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,40 @@
+import { notFound } from "next/navigation";
+import { routing } from "@/i18n/routing";
+import MenuBar from "@/components/nav-menu";
+
+import type { Metadata } from "next";
+import { NextIntlClientProvider, hasLocale } from "next-intl";
+import { getTranslations } from "next-intl/server";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}): Promise<Metadata> {
+  const { locale } = await params;
+  const t = await getTranslations({ locale, namespace: "Metadata" });
+  return {
+    title: t("title"),
+    description: t("description"),
+  };
+}
+
+export default async function LocaleLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  return (
+    <NextIntlClientProvider>
+      <main>{children}</main>
+      <MenuBar />
+    </NextIntlClientProvider>
+  );
+}
