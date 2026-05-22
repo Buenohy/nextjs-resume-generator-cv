ac15210ab6ec2fc924149dccf190b1ae28bae580
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-05-22 16:00:58 -0300

    feat: Changed style name page in jon-description page

--- src/app/[locale]/(builder)/job-description/page.tsx
+++ src/app/[locale]/(builder)/job-description/page.tsx
@@ -17,23 +17,14 @@ import { useRouter } from "next/navigation";
 export default function JobDescriptionPage() {
   const router = useRouter();
 
-  const handleBackStep = () => {
-    router.push("/resume-builder");
-  };
-
   const handleNextStep = () => {
     router.push("/resume-builder");
   };
   return (
-    <div>
-      <div>Job Description Page</div>
+    <div className="p-6">
+      <h1 className="mb-6 text-2xl font-bold">Job Description</h1>
       <Card className="shadow-primary/50 shadow-lg">
-        <div className="flex items-center justify-between px-6">
-          <CardAction>
-            <Button disabled onClick={handleBackStep}>
-              Dados Currículo
-            </Button>
-          </CardAction>
+        <div className="flex items-center justify-end px-6">
           <CardAction>
             <Button onClick={handleNextStep}>Dados Currículo</Button>
           </CardAction>
