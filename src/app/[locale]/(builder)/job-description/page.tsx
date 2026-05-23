6460d26de0105228c01b780402b46571fceea2b5
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-05-22 22:13:46 -0300

    feat: Ajust page to save text and save in zustand global store

--- src/app/[locale]/(builder)/job-description/page.tsx
+++ src/app/[locale]/(builder)/job-description/page.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { useState } from "react";
+import { useRouter } from "@/i18n/navigation";
+import { useResumeStore } from "@/store/useResumeStore";
+
 import ButtonPaginate from "@/components/button-paginate";
 import { Button } from "@/components/ui/button";
 import {
@@ -14,8 +18,19 @@ import {
 import { Textarea } from "@/components/ui/textarea";
 
 export default function JobDescriptionPage() {
+  const router = useRouter();
+  const globalJobText = useResumeStore((state) => state.jobText);
+  const setGlobalJobText = useResumeStore((state) => state.setJobText);
+
+  const [localText, setLocalText] = useState(globalJobText);
+
+  const handleSubmit = () => {
+    setGlobalJobText(localText);
+    router.push("/resume-builder");
+  };
+
   return (
-    <div className="p-3 sm:p-6">
+    <div className="p-4 sm:p-6">
       <h1 className="mb-6 text-2xl font-bold">Job Description</h1>
       <Card className="shadow-primary/50 shadow-lg">
         <CardHeader>
@@ -25,15 +40,20 @@ export default function JobDescriptionPage() {
         <CardContent>
           <Textarea
             placeholder="Cole a descrição completa da vaga do Wellfound aqui..."
-            className="sm:min-h-30 xl:min-h-60"
+            value={localText}
+            onChange={(e) => setLocalText(e.target.value)}
+            className="sm:min-h-62.5"
           />
         </CardContent>
         <CardFooter className="flex flex-col gap-6">
           <div className="flex w-full justify-center">
             <CardAction>
-              <Button>Enviar descrição da vaga</Button>
+              <Button onClick={handleSubmit} disabled={!localText.trim()}>
+                Enviar descrição da vaga
+              </Button>
             </CardAction>
           </div>
+          <ButtonPaginate />
         </CardFooter>
       </Card>
     </div>
