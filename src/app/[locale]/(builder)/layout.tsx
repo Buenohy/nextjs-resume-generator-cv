e6df16a6a8a46f1228a6bd7fecc2f2999ebb58aa
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-05-24 10:25:24 -0300

    feat: Add next-intl in layout.tsx builder

--- src/app/[locale]/(builder)/layout.tsx
+++ src/app/[locale]/(builder)/layout.tsx
@@ -1,4 +1,5 @@
-import ButtonPaginate from "@/components/pagination-buttons";
+import { useTranslations } from "next-intl";
+import PaginationButtons from "@/components/pagination-buttons";
 import { Stepper } from "@/components/stepper";
 import { Card, CardFooter } from "@/components/ui/card";
 
@@ -7,17 +8,16 @@ export default function BuilderLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const t = useTranslations("BuilderLayout");
+
   return (
     <div className="bg-muted/30 min-h-screen pt-8 pb-12">
       <div className="max-w-8xl mx-auto sm:px-6">
         <div className="mb-8 flex flex-col gap-2">
           <h1 className="text-foreground text-3xl font-bold tracking-tight">
-            Gerador de Currículo ATS
+            {t("title")}
           </h1>
-          <p className="text-muted-foreground">
-            Siga os passos abaixo para analisar e otimizar seu currículo para a
-            vaga.
-          </p>
+          <p className="text-muted-foreground">{t("subtitle")}</p>
         </div>
 
         <div className="mb-12">
@@ -27,7 +27,7 @@ export default function BuilderLayout({
         <Card className="border-muted bg-background shadow-primary/5 p-3 shadow-lg sm:p-6">
           {children}
           <CardFooter className="flex flex-col gap-6">
-            <ButtonPaginate />
+            <PaginationButtons />
           </CardFooter>
         </Card>
       </div>
