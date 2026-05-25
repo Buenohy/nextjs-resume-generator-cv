eecbe98d7b96ed8f4cff85ae1e9b0b5cc140dfd9
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-05-24 21:58:35 -0300

    feat: Changed tailwindCSS theme

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   const t = useTranslations("HomePage");
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
-      <main className="flex h-screen w-full flex-col items-center justify-center overflow-hidden bg-white p-4 sm:items-start">
+    <div className="bg-background flex min-h-screen items-center justify-center font-sans">
+      <main className="flex h-screen w-full flex-col items-center justify-center overflow-hidden p-4 sm:items-start">
         <div>{t("title")}</div>
         <ButtonPaginate />
       </main>
