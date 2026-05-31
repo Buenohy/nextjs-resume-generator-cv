18799a01696adf964ebb62575fc1b70a3f51c79f
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-05-31 18:19:28 -0300

    feat: header-section using useSyncCollapse custom hook

--- src/components/sections/header-section.tsx
+++ src/components/sections/header-section.tsx
@@ -15,14 +15,16 @@ import {
   CollapsibleTrigger,
 } from "@/components/ui/collapsible";
 
+import { useSyncCollapse } from "@/app/hooks/useSyncCollapse";
+
 export function HeaderSection() {
   const t = useTranslations("ResumeBuilderPage");
   const cvData = useResumeStore((s) => s.cvData);
   const updateCvData = useResumeStore((s) => s.updateCvData);
 
   const [isMounted, setIsMounted] = useState(false);
-  const [isOpen, setIsOpen] = useState(true);
 
+  const [isOpen, setIsOpen] = useSyncCollapse("personal-info", true);
   {
     /* 
     DEFERRED MOUNT EFFECT
