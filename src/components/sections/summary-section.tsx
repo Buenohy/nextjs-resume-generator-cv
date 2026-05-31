76b04a4f3280e116033bc2933260380d4673afee
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-05-31 18:21:26 -0300

    feat: summary-section using useSyncCollapse custom hook

--- src/components/sections/summary-section.tsx
+++ src/components/sections/summary-section.tsx
@@ -16,6 +16,8 @@ import {
   CollapsibleTrigger,
 } from "@/components/ui/collapsible";
 
+import { useSyncCollapse } from "@/app/hooks/useSyncCollapse";
+
 export function SummarySection() {
   const t = useTranslations("ResumeBuilderPage");
   const cvData = useResumeStore((s) => s.cvData);
@@ -23,8 +25,8 @@ export function SummarySection() {
   const handleAutoResize = useAutoResize();
 
   const [isMounted, setIsMounted] = useState(false);
-  const [isOpen, setIsOpen] = useState(true);
 
+  const [isOpen, setIsOpen] = useSyncCollapse("summary", true);
   {
     /* 
     DEFERRED MOUNT EFFECT
