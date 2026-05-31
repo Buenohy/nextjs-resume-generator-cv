1060e95a502c0ff01af0b2e2c9a6b8ad404c1aec
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-05-31 16:18:05 -0300

    feat: Config eslint with plugins

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,26 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import betterTailwindcss from "eslint-plugin-better-tailwindcss";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
+  {
+    plugins: {
+      "better-tailwindcss": betterTailwindcss,
+    },
+    rules: {
+      ...betterTailwindcss.configs.recommended.rules,
+    },
+    settings: {
+      tailwindcss: {
+        callees: ["cn", "clsx", "classNames"],
+      },
+    },
+  },
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
