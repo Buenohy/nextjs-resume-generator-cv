9c4e3280fd2156c0f78f65f7cdc4b17eff63447b
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-06-10 14:41:39 -0300

    feat: Changed icons in resume.tsx

--- src/components/resume.tsx
+++ src/components/resume.tsx
@@ -8,6 +8,8 @@ import {
   StyleSheet,
   Svg,
   Path,
+  Rect,
+  Circle,
 } from "@react-pdf/renderer";
 
 export interface Info {
@@ -241,12 +243,105 @@ const styles = StyleSheet.create({
   },
 });
 
-const Icon = ({ path }: { path: string }) => (
+//Icons (size standardized in 9x9)
+
+const LinkedinIcon = () => (
   <Svg
     viewBox="0 0 24 24"
     style={{ width: 9, height: 9, marginRight: 3, alignSelf: "center" }}
   >
-    <Path fill="#10b981" d={path} />
+    <Path
+      fill="#10b981"
+      d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.79M6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37h2.77z"
+    />
+  </Svg>
+);
+
+const PhoneIcon = () => (
+  <Svg
+    viewBox="0 0 24 24"
+    style={{ width: 9, height: 9, marginRight: 3, alignSelf: "center" }}
+  >
+    <Path
+      fill="#10b981"
+      d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z"
+    />
+  </Svg>
+);
+
+const WebsiteIcon = () => (
+  <Svg
+    viewBox="0 0 24 24"
+    style={{ width: 9, height: 9, marginRight: 3, alignSelf: "center" }}
+  >
+    {/* Círculo Externo */}
+    <Circle
+      cx="12"
+      cy="12"
+      r="10"
+      fill="none"
+      stroke="#10b981"
+      strokeWidth="1.5"
+    />
+    {/* Elipses Longitudinais Verticais */}
+    <Path
+      fill="none"
+      stroke="#10b981"
+      strokeWidth="1.2"
+      d="M12 2a5 10 0 0 0 0 20 5 10 0 0 0 0-20z"
+    />
+    {/* Linha Vertical Central */}
+    <Path fill="none" stroke="#10b981" strokeWidth="1.2" d="M12 2v20" />
+    {/* Linha Horizontal do Equador */}
+    <Path fill="none" stroke="#10b981" strokeWidth="1.2" d="M2 12h20" />
+    {/* Linhas Horizontais Latitudinais (Curvadas) */}
+    <Path
+      fill="none"
+      stroke="#10b981"
+      strokeWidth="1.2"
+      d="M3.8 7a18 18 0 0 1 16.4 0M3.8 17a18 18 0 0 0 16.4 0"
+    />
+  </Svg>
+);
+
+const EmailIcon = () => (
+  <Svg
+    viewBox="0 0 24 24"
+    style={{ width: 9, height: 9, marginRight: 3, alignSelf: "center" }}
+  >
+    <Path
+      fill="#10b981"
+      d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"
+    />
+  </Svg>
+);
+
+const GithubIcon = () => (
+  <Svg
+    viewBox="0 0 24 24"
+    style={{ width: 9, height: 9, marginRight: 3, alignSelf: "center" }}
+  >
+    <Path
+      fill="#10b981"
+      d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34-.46-1.16-1.11-1.47-1.11-1.47-.91-.62.07-.6.07-.6 1 .07 1.53 1.03 1.53 1.03.87 1.52 2.34 1.07 2.91.83.09-.65.35-1.09.63-1.34-2.22-.25-4.55-1.11-4.55-4.92 0-1.11.38-2 1.03-2.71-.1-.25-.45-1.29.1-2.64 0 0 .84-.27 2.75 1.02.79-.22 1.65-.33 2.5-.33.85 0 1.71.11 2.5.33 1.91-1.29 2.75-1.02 2.75-1.02.55 1.35.2 2.39.1 2.64.65.71 1.03 1.6 1.03 2.71 0 3.82-2.34 4.66-4.57 4.91.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12a10 10 0 0 0-12-10z"
+    />
+  </Svg>
+);
+
+const ExternalLinkIcon = ({ isTrailing = false }: { isTrailing?: boolean }) => (
+  <Svg
+    viewBox="0 0 24 24"
+    style={{
+      width: 9,
+      height: 9,
+      marginLeft: isTrailing ? 2 : 3,
+      alignSelf: "center",
+    }}
+  >
+    <Path
+      fill="#10b981"
+      d="M14 3v2h3.59L7.76 14.83l1.41 1.41L19 6.41V10h2V3M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7z"
+    />
   </Svg>
 );
 
@@ -306,34 +401,39 @@ export const Resume: React.FC<ResumeProps> = ({
             <View style={styles.contactGroupLeft}>
               {info.linkedin && (
                 <Link href={info.linkedin_url || ""} style={styles.contactLink}>
-                  <Icon path="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.79M6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37h2.77z" />
+                  <LinkedinIcon />
                   <Text style={styles.linkText}>{info.linkedin}</Text>
+                  <ExternalLinkIcon isTrailing={true} />
                 </Link>
               )}
               {info.phone && (
                 <Link href={info.phone_url || ""} style={styles.contactLink}>
-                  <Icon path="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z" />
+                  <PhoneIcon />
                   <Text style={styles.linkText}>{info.phone}</Text>
+                  <ExternalLinkIcon isTrailing={true} />
                 </Link>
               )}
             </View>
             <View style={styles.contactGroupCenter}>
               {info.website && (
                 <Link href={info.website_url || ""} style={styles.contactLink}>
-                  <Icon path="M12 2a10 10 0 0 1 10 10 10 10 0 0 1-10 10A10 10 0 0 1 2 12 10 10 0 0 1 12 2m0 2a8 8 0 0 0-8 8c0 1.86.64 3.57 1.71 4.94l.89-.9c.7-.7.96-1.09.96-1.54 0-.41-.31-.83-.96-1.5l-1.35-1.34V8l2.5 2.5v2.5l2.5-2.5V8.5l-2-2H9v-2h2v1l3 3 2-2h1.34c.43.95.66 2 .66 3.1 0 1.67-.53 3.23-1.44 4.5l-1.56-1.56c-.25-.26-.64-.26-.9 0-.25.26-.25.64 0 .9l1.64 1.63A7.95 7.95 0 0 0 20 12a8 8 0 0 0-8-8z" />
+                  <WebsiteIcon />
                   <Text style={styles.linkText}>{info.website}</Text>
+                  <ExternalLinkIcon isTrailing={true} />
                 </Link>
               )}
             </View>
             <View style={styles.contactGroupRight}>
               <Link href={`mailto:${info.email}`} style={styles.contactLink}>
-                <Icon path="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
+                <EmailIcon />
                 <Text style={styles.linkText}>{info.email}</Text>
+                <ExternalLinkIcon isTrailing={true} />
               </Link>
               {info.github && (
                 <Link href={info.github_url || ""} style={styles.contactLink}>
-                  <Icon path="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34-.46-1.16-1.11-1.47-1.11-1.47-.91-.62.07-.6.07-.6 1 .07 1.53 1.03 1.53 1.03.87 1.52 2.34 1.07 2.91.83.09-.65.35-1.09.63-1.34-2.22-.25-4.55-1.11-4.55-4.92 0-1.11.38-2 1.03-2.71-.1-.25-.45-1.29.1-2.64 0 0 .84-.27 2.75 1.02.79-.22 1.65-.33 2.5-.33.85 0 1.71.11 2.5.33 1.91-1.29 2.75-1.02 2.75-1.02.55 1.35.2 2.39.1 2.64.65.71 1.03 1.6 1.03 2.71 0 3.82-2.34 4.66-4.57 4.91.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12a10 10 0 0 0-12-10z" />
+                  <GithubIcon />
                   <Text style={styles.linkText}>{info.github}</Text>
+                  <ExternalLinkIcon isTrailing={true} />
                 </Link>
               )}
             </View>
@@ -380,15 +480,7 @@ export const Resume: React.FC<ResumeProps> = ({
                     <Text style={{ textDecoration: "underline" }}>
                       {job.company}
                     </Text>
-                    <Svg
-                      viewBox="0 0 24 24"
-                      style={{ width: 7, height: 7, marginLeft: 3 }}
-                    >
-                      <Path
-                        fill="#10b981"
-                        d="M14 3v2h3.59L7.76 14.83l1.41 1.41L19 6.41V10h2V3M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7z"
-                      />
-                    </Svg>
+                    <ExternalLinkIcon />
                   </Link>
                 ) : (
                   <Text style={styles.experienceCompanyFallback}>
