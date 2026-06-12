ed40489267c15970d82a9dac0e50e70d7c6486b0
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-06-12 15:13:28 -0300

    feat: Add comments theme-provider.tsx

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,11 +1,15 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
+/**
+ * ThemeProvider wraps the application to enable dynamic light, dark, and system theme switching
+ * using the next-themes library.
+ */
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
